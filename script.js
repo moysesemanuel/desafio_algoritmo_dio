2866a1f1b50e2cce6527bbc043749c38383908c7
Moyses Emanuel <164270978+moysesemanuel@users.noreply.github.com>
2024-10-04 15:13:52 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+const frase = "cadastrado com sucesso"
+let nomesPokemon = ["Poochyena" , "Zigzagoon" , "Dragonite" , "Wurwple"]
+let timePokemon = [
+  ["Poochyena", 2 , "M", "Hp 13/13"],
+  ["Zigzagoon", 2 , "F", "Hp 13/13"],
+  ["Dragonite", 5 , "M", "Hp 25/25"],
+  ["Wurwple", 2 , "M", "Hp 7/14"]
+]
+
+
+console.log(timePokemon[3][0] + " " + frase)
